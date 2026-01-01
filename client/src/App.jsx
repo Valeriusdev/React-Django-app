4127051e13eddc8d1ddf8c0d8ac215a127831008
Val Lysenko <kakao757@gmail.com>
2025-12-31 20:34:04 -0800

    wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1 className="text-blue-500">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-blue-500">Book app</h1>
+      <div>
+        <input type="text" placeholder="Book title..." />
+        <input type="text" placeholder="Release Date..." />
+        <button>Add Book</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
