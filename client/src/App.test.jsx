7903fcbf88eed9a54c3e14debc5ea3fd327d27b6
Val Lysenko <kakao757@gmail.com>
2026-02-07 20:27:08 -0800

    add a Jest test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -12,4 +12,10 @@ describe("App", () => {
     const heading = screen.getByText(/Book app/i);
     expect(heading).toBeInTheDocument();
   });
+
+  test("renders Add Book button", () => {
+    render(<App />);
+    const addButton = screen.getByRole("button", { name: /Add Book/i });
+    expect(addButton).toBeInTheDocument();
+  });
 });
