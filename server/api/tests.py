06151567852be18e82e713e363f5fffb0953c0e2
Val Lysenko <kakao757@gmail.com>
2026-05-23 20:07:15 -0700

    add get books endpoint pytest test

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+from api.models import Book
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+def test_get_books_returns_200(api_client):
+    response = api_client.get(reverse('get_books'))
+    assert response.status_code == 200
