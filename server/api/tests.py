93a0abd357c395eb288dc1096915607730051bb5
Val Lysenko <kakao757@gmail.com>
2026-05-25 22:53:44 -0700

    add pytest test for get books endpoint

--- server/api/tests.py
+++ server/api/tests.py
@@ -13,3 +13,12 @@ def api_client():
 def test_get_books_returns_200(api_client):
     response = api_client.get(reverse('get_books'))
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_get_books_returns_list(api_client):
+    Book.objects.create(title='Test Book', release_year=2026)
+    response = api_client.get(reverse('get_books'))
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 1
+    assert data[0]['title'] == 'Test Book'
